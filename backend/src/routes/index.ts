3a79154b99944c6129c8063af4d431a239c593fa
Maicon Silva <maiconrs95@gmail.com>
2020-04-19 03:26:19 -0300

    update: eslint/prettier

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,10 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', ((request, response) => response.json({ ok: true })));
+routes.get('/', (request, response) =>
+    response.json({
+        ok: true,
+    }),
+);
 
 export default routes;
