c98a6cd00c7a64f33de300e6f04fd267bc104c97
Maicon Silva <maiconrs95@gmail.com>
2020-05-02 16:16:56 -0300

    add: bcrypt to encrypt user password

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -15,6 +15,8 @@ usersRouter.post('/', async (request, response) => {
             password,
         });
 
+        delete user.password;
+
         return response.json(user);
     } catch ({ message }) {
         return response.status(400).json({ message });
