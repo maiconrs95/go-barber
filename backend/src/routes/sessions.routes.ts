67670448d1ef470d5edbe5eedcd50f8a87d3ce9f
Maicon Silva <maiconrs95@gmail.com>
2020-05-03 01:13:27 -0300

    add: default error handler

--- backend/src/routes/sessions.routes.ts
+++ backend/src/routes/sessions.routes.ts
@@ -5,20 +5,16 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-    try {
-        const { email, password } = request.body;
+    const { email, password } = request.body;
 
-        const authenticateUserService = new AuthenticateUserService();
+    const authenticateUserService = new AuthenticateUserService();
 
-        const { user, token } = await authenticateUserService.execute({
-            email,
-            password,
-        });
+    const { user, token } = await authenticateUserService.execute({
+        email,
+        password,
+    });
 
-        return response.json({ user, token });
-    } catch ({ message }) {
-        return response.status(400).json({ message });
-    }
+    return response.json({ user, token });
 });
 
 export default sessionsRouter;
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -4,23 +4,19 @@ import CreateUserService from '../services/CreateUserService';
 const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
-    try {
-        const { name, email, password } = request.body;
+    const { name, email, password } = request.body;
 
-        const createUser = new CreateUserService();
+    const createUser = new CreateUserService();
 
-        const user = await createUser.execute({
-            name,
-            email,
-            password,
-        });
+    const user = await createUser.execute({
+        name,
+        email,
+        password,
+    });
 
-        delete user.password;
+    delete user.password;
 
-        return response.json(user);
-    } catch ({ message }) {
-        return response.status(400).json({ message });
-    }
+    return response.json(user);
 });
 
 export default usersRouter;
