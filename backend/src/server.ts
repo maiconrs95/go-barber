5b2edd9e9e0aeea7289b88ddd38d61d5b55ea793
Maicon Silva <maiconrs95@gmail.com>
2020-05-03 02:08:45 -0300

    update: route to static files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import routes from './routes';
+import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 
 import './database';
@@ -10,6 +11,7 @@ import './database';
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
