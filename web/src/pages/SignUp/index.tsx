311de67bb93d39b0e03e6f68cfdbcd9772dabb31
Maicon Silva <maiconrs95@gmail.com>
2020-05-06 13:04:06 -0300

    add: signup page

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiMail, FiLock, FiLogIn } from 'react-icons/fi';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -12,12 +12,19 @@ import { Container, Content, Background } from './styles';
 const SignIn: React.FC = () => {
     return (
         <Container>
+            <Background />
             <Content>
                 <img src={logoImg} alt="Go-Barber" />
 
                 <form>
                     <h1>Faça seu logon</h1>
 
+                    <Input
+                        icon={FiUser}
+                        placeholder="Nome"
+                        type="text"
+                        name="name"
+                    />
                     <Input
                         icon={FiMail}
                         placeholder="E-mail"
@@ -32,15 +39,12 @@ const SignIn: React.FC = () => {
                     />
 
                     <Button type="submit">Entrar</Button>
-
-                    <a href="void(0)">Esqueci minha senha</a>
                 </form>
-                <Link to="/signup">
-                    <FiLogIn />
-                    Criar conta
+                <Link to="/">
+                    <FiArrowLeft />
+                    Já possuo uma conta
                 </Link>
             </Content>
-            <Background />
         </Container>
     );
 };
