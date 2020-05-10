e919c2b4533dc61844bee4baea421e2ed6e38f06
Maicon Silva <maiconrs95@gmail.com>
2020-05-10 03:58:25 -0300

    add: private routes component

---
+++ web/src/routes/Route.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+    Route as ReactDOMRoute,
+    RouteProps as ReactDOMProps,
+    Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDOMProps {
+    isPrivate?: boolean;
+    component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+    isPrivate = false,
+    component: Component,
+    ...rest
+}) => {
+    const { user } = useAuth();
+    const isSigned = !!user;
+
+    return (
+        <ReactDOMRoute
+            {...rest}
+            render={({ location }) => {
+                return isPrivate === isSigned ? (
+                    <Component />
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: isPrivate ? '/' : '/dashboard',
+                            state: {
+                                from: location,
+                            },
+                        }}
+                    />
+                );
+            }}
+        />
+    );
+};
+
+export default Route;
